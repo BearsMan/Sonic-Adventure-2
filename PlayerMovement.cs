c591acc46661793d8b3cb981d658606000cc6f1a
Darius Ronkowitz <dronkowitz@yahoo.com>
2024-09-19 11:17:53 -0400

    `Added CameraController and PlayerMovement.cs`
    Added basic player movement and camera movement.

---
+++ PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed = 1f;
+    public float runSpeed = 5f;
+    public float jumpForce = 7f;
+    public float gravity = 10f;
+    public float jumpSpeed = 3f;
+    public float jumpHeight = 4f;
+    public float verticalVelocityMovement;
+    public float horizontalVelocityMovement;    
+    public CharacterController controller;
+    private bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 moveDir = new Vector3(verticalVelocityMovement, horizontalVelocityMovement).normalized;
+        float horizontalVelocity = controller.velocity.x;
+        float verticalVelocity = controller.velocity.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
